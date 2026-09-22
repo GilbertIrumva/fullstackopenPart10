2ff568ff7c399b595a16c0a9210442e074c94cea
Gilbertirumva <167775136+GilbertIrumva@users.noreply.github.com>
2026-09-22 17:32:13 +0300

    complete exercise 10 platform specific font

--- components/Text.jsx
+++ components/Text.jsx
@@ -1,7 +1,8 @@
 import { Text as NativeText } from 'react-native';
+import theme from '../theme';
 
 const Text = (props) => {
-  return <NativeText {...props} />;
+  return <NativeText {...props} style={[{ fontFamily: theme.fonts.main }, props.style]} />;
 };
 
 export default Text;
